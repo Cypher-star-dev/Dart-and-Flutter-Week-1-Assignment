6c2d6081c7ca23b4a67574190de3609cea693d77
Cypher-star-dev <omogagikonyo31@gmail.com>
2025-06-02 23:08:02 +0300

    Create answers.dart

---
+++ answers.dart
@@ -0,0 +1,9 @@
+Question 1 answer.
+
+  void main() {
+  int rolls = 22;
+  double dices = 44.8;
+  String name = "Titanic";
+  bool didSink = false;
+  List ourList = [88, 97, "Earth", "Mexico", 99.7]
+}
